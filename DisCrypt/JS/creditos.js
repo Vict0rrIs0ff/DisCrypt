75ac94d5afe83b2f4d751de3f720796ae16c5599
José Valdez <valdez.josejoaquim@gmail.com>
2024-12-08 02:48:36 -0300

    DisCrypt

---
+++ DisCrypt/JS/creditos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const creditsDuration = 20000; // 15s + 2s de fade-in
+    setTimeout(() => {
+        window.location.href = "index.html"; // Redirecione para a página inicial ou outro destino
+    }, creditsDuration);
+});
