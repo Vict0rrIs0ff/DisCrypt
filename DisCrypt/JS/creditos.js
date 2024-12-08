493a043b7b8064145f0be31f2c69995e13201b47
José Valdez <valdez.josejoaquim@gmail.com>
2024-12-08 02:47:14 -0300

    Delete DisCrypt directory

--- DisCrypt/JS/creditos.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const creditsDuration = 20000; // 15s + 2s de fade-in
-    setTimeout(() => {
-        window.location.href = "index.html"; // Redirecione para a página inicial ou outro destino
-    }, creditsDuration);
-});
