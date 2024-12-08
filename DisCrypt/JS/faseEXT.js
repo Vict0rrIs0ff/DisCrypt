07b82a03bed6a3f00a454d9c2a31704065e9b7ec
José Valdez <valdez.josejoaquim@gmail.com>
2024-12-08 02:44:46 -0300

    DesCrypt

---
+++ DisCrypt/JS/faseEXT.js
@@ -0,0 +1,18 @@
+function askPassword() {
+    const password = prompt(" ");
+    if (password === null) {
+        return;
+    }
+    if (password === "JORGE") {
+        window.location.href = "creditos.html";
+    }
+    else if (password === "jorge") {
+        window.location.href = "creditos.html";
+    } else {
+        alert("Senha incorreta. Desista enquanto pode.");
+    }
+}
+
+function showHint() {
+    alert("K C U F N 1 4 R B");
+}
---
+++ DisCrypt/JS/global.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const phase = document.body.dataset.phase;
+    console.log(`Fase atual: ${phase}`);
+});
